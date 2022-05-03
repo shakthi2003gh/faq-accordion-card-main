14e632c31e2ca9cbbf8d3f5c3340fc312371817d
shakthi2003gh <shakthikumar13456789@gmail.com>
2022-05-03 17:30:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const listHeading = document.querySelectorAll(".list__heading");
+
+listHeading.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    const contentList = e.target.closest(".content__list");
+    const tragetedlistHeading = contentList.querySelector(".list__heading");
+    const listContent = contentList.querySelector(".list__content");
+
+    tragetedlistHeading.classList.toggle("heading--hover");
+    tragetedlistHeading.children[1].classList.toggle("expanded-button");
+    listContent.classList.toggle("list__content-expand");
+
+    listHeading.forEach((element) => {
+      if (element.innerHTML != tragetedlistHeading.innerHTML) {
+        element.classList.remove("heading--hover");
+        element.children[1].classList.remove("expanded-button");
+        element.nextElementSibling.classList.remove("list__content-expand");
+      }
+    });
+  });
+});
